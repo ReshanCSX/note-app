02a8661ae43a3d64539614eca76e89381eeaae82
ReshanCSX <reshan.luck@gmail.com>
2023-08-14 19:53:04 +0530

    added note names

--- note-app/src/components/Sidebar.jsx
+++ note-app/src/components/Sidebar.jsx
@@ -1,4 +1,3 @@
-import React from "react"
 
 export default function Sidebar(props) {
     const noteElements = props.notes.map((note, index) => (
@@ -10,7 +9,7 @@ export default function Sidebar(props) {
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
             </div>
         </div>
     ))
